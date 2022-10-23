f99011b022f491d2aa858b6e1ac68de9cdd65199
LucasD1az <110778622+LucasD1az@users.noreply.github.com>
2022-10-23 01:01:17 -0300

    Started routing the project

--- Proyecto/FrontEnd/src/app/service/person.service.ts
+++ Proyecto/FrontEnd/src/app/service/person.service.ts
@@ -7,7 +7,7 @@ import { person } from '../model/person.model';
   providedIn: 'root'
 })
 export class PersonService {
-  URL = 'http://localhost:8080/personas';
+  URL = 'http://localhost:8080/personas/';
   constructor(private http: HttpClient) { }
 
   public getPerson(): Observable<person>{
